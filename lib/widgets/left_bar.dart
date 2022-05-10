83575d2201cba258a0699a83aef42fb37f5bebca
wwjingwweii <wjingweiiiw@gmail.com>
2022-05-10 17:31:31 +0800

    first commit

---
+++ lib/widgets/left_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task1/constants/app_constants.dart';
+
+class LeftBar extends StatelessWidget {
+  final double barWidth;
+
+  const LeftBar({Key key, @required this.barWidth})
+      : super(key: key); //constructor
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      //align everything to the right side
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+              color: accentHexColor),
+        )
+      ],
+    );
+  }
+}
